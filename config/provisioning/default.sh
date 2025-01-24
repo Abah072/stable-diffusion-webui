e3dae6f0dc66b448d01d5ceed6df2fee2b9ada04
Abah072 <65512379+Abah072@users.noreply.github.com>
2025-01-24 22:35:19 +0700

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -28,17 +28,21 @@ EXTENSIONS=(
     "https://github.com/hako-mikan/sd-webui-regional-prompter"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
     "https://github.com/Gourieff/sd-webui-reactor"
+    "https://github.com/Iyashinouta/sd-model-downloader"
 )
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
-    #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
+    #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    "https://civitai.com/api/download/models/1047139?type=Model&format=SafeTensor&size=pruned&fp=fp16"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
 )
 
 LORA_MODELS=(
-    #"https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/382152?type=Model&format=SafeTensor"
+    "https://civitai.com/api/download/models/556144?type=Model&format=SafeTensor"
+    "https://civitai.com/api/download/models/691705?type=Model&format=SafeTensor"
+    "https://civitai.com/api/download/models/1007333?type=Model&format=SafeTensor"
 )
 
 VAE_MODELS=(
